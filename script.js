647782762be5705389406a65c6111022cbe78efe
dnyanraj09 <dnyanrajdhondge927@gmail.com>
2024-07-24 20:07:17 +0530

    add new code

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-document.getElementById('btnPreview').addEventListener('click', function() {
-    document.getElementById('previewModal').style.display = 'block';
-  });
-  
-  document.getElementById('btnClose').addEventListener('click', function() {
-    document.getElementById('previewModal').style.display = 'none';
-  });
-  
-  window.addEventListener('click', function(event) {
-    var modal = document.getElementById('previewModal');
-    if (event.target == modal) {
-      modal.style.display = 'none';
-    }
-  });
-  
+function changeImage(imageName) {
+    const mainImage = document.getElementById('main-product-image');
+    mainImage.src = imageName;
+}
